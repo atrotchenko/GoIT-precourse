31d175d67a2de744e32484919de94c1a1858a460
atrotchenko <andrey.trotchenko@gmail.com>
2017-04-01 22:05:58 +0300

    В переменной n хранится вещественное число с ненулевой дробной частью. Создайте программу, которая округляет число n до ближайшего целого и выводящую результат на экран.

---
+++ src/pre13.java
@@ -0,0 +1,16 @@
+/**
+ * Created by victorpetrov on 01.04.17.
+ */
+public class pre13 {
+    public static void main(String[] args) {
+
+        /*В переменной n хранится вещественное число с ненулевой дробной частью. Создайте программу,
+        которая округляет число n до ближайшего целого и выводящую результат на экран.*/
+
+        double n = 1.5;
+
+        int m = (int)(n + 0.5);
+
+        System.out.println(m);
+    }
+}
