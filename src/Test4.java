698eae98acd3341145d943a608eac802c0c970bf
atrotchenko <andrey.trotchenko@gmail.com>
2017-04-01 21:41:13 +0300

    Initial commit

---
+++ src/Test4.java
@@ -0,0 +1,25 @@
+/**
+ * Created by victorpetrov on 31.03.17.
+ */
+public class Test4 {
+
+    public static void main(String[] args) {
+
+      /*  В переменных q и w хранятся два натуральных числа. Создайте программу, выводящую на экран результат деления q на w с остатком.
+                Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8):21 / 8 = 2 и 5 в остатке
+*/
+
+
+      int q = 21;
+
+      int w = 8;
+
+      int x = q/w;
+
+
+        System.out.println(x);
+        System.out.println(q%8);
+
+    }
+
+}
